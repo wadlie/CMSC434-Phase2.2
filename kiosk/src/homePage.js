bdab861e53f884b1aa2d33170e6735d5eb803d24
wadlie <32208408+wadlie@users.noreply.github.com>
2019-04-18 20:26:17 -0400

    Finished up Home page
    Added home page image and two footers that change every 10 seconds. One is a "fun fact" other is a contact page. Best of both world of design

--- kiosk/src/homePage.js
+++ kiosk/src/homePage.js
@@ -1,19 +1,58 @@
 import React, {Component} from "react";
 //import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
+import homeImage from './dist/images/HomeScreen.png';
+import contactImage from './dist/images/Contact.png';
+import iribe from './dist/images/BrendanIribe.png';
+
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            images: [
+                iribe,
+                contactImage
+            ],
+            selectedImage: iribe
+        };
+    }
 
 
+    componentDidMount() {
+       this.interval = setInterval(() => {
+            this.setState(prevState => {
+                if (prevState.selectedImage === this.state.images[0]) {
+                    return {
+                        selectedImage: this.state.images[1]
+                    };
+                } else {
+                    return {
+                        selectedImage: this.state.images[0]
+                    };
+                }
+            });
+        }, 10000);
+    }
+componentWillUnmount() {
+    clearInterval(this.interval);
+}
     render() {
         var displayStyle = {
-            display: "block",
             weight: "100%",
             height: "100%",
         }
+
         return (
-            <div style={displayStyle}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpq8T9bdcQpy2myWNZhLI5m6YGbyugIPV-FY26kj17-W9Jo6Kj" alt="PH"></img>
+            <div style={displayStyle} className="container-fluid">
+                <div className="row" style={{height:"70%",padding:10}}>
+                <img src={homeImage} style ={{maxWidth:"100%",maxHeight:"100%"}}></img>
+                </div>
+                <div className="row" style={{height:"30%",backgroundColor:"red"}}>
+                    <img src={this.state.selectedImage} alt="" style ={{maxWidth:"100%",maxHeight:"100%"}}></img>
+
+                </div>
             </div>
         );
     }
