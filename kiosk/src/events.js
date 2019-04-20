d9fd4ca20b5e6eca46503573b215d6bf3f4a4b41
Alex Ma <alex555ma@gmail.com>
2019-04-20 15:31:34 -0400

    implemented events page using structure of rooms page, has two events, only one functional (pizza one)

--- kiosk/src/events.js
+++ kiosk/src/events.js
@@ -1,58 +1,132 @@
 import React, {Component} from "react";
 //import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
+import "./index.css";
+import eventScreen from './dist/eventimgs/pizzaParty.jpg';
+import pizzaTime from './dist/eventimgs/pizzaTime.png';
+import speakerTime from './dist/eventimgs/guestSpeakerTime.png';
 
 class Events extends Component {
 	
-	goToEvent(){
-		/*
-			do something with an image change.
-		*/
-		document.getElementById('calendar_img').setAttribute('src', 'http://clipart-library.com/data_images/436942.png')
-		document.getElementById("pizza_button").style.display = "none";
-  }
+	  constructor(props) {
+        super(props);
+
+        this.state = {
+            from: "",
+            to: "",
+            room: false,
+            list: [{key:"Pizza Party",value:pizzaTime},{key:"Guest Speaker",value:speakerTime}]
+            //list: [image00]
+        };
+        this.handleChangeFrom = this.handleChangeFrom.bind(this);
+        this.handleChangeTo = this.handleChangeTo.bind(this);
+        this.handleRoomBoolean = this.handleRoomBoolean.bind(this);
+        this.goBack = this.goBack.bind(this)
+    }
 	
+	handleChangeFrom(event) {
+        if (event.target.value === ""){
+            this.setState({list: [{key:"Pizza Party",value:pizzaTime},{key:"Guest Speaker",value:speakerTime}]})
+        } else {
+            this.setState({list:[{key:"Guest Speaker",value:speakerTime}]});
+        }
+        this.setState({from: event.target.value});
+    }
 	
+	handleChangeTo(event) {
+        this.setState({to: event.target.value});
+    }
+    handleRoomBoolean(){
+        this.setState({room: true});
+    }
+    goBack(){
+        this.setState({room:false});
+    }
+
+    showPop(){
+        if (this.state.room === true){
+            return (
+                <div style={{display:"block",width:"100%",height:"100%"}} className="container">
+                    <div className="row"style={{height:"10%",paddingTop:"5px"}}>
+                        <div className="col-1" style={{paddingLeft: 1}}>
+                            <button onClick={this.goBack} className="backBtn"><i style={{fontSize:"2em"}} className="fa fa-arrow-left"></i> </button>
+                        </div>
+                        <div className="col-3"></div>
+                        <div className="col-3" style={{fontSize: "1.5em",fontFamily:"sans-sherif",paddingLeft:30,paddingRight:0}}>IRB 1207</div>
+                        <div className="col-4"></div>
+                        <div className="col-1"><i style={{fontSize:"2em"}} disabled className="fa fa-info-circle" data-toggle="tooltip" data-placement="top" title="Drag and drop time section to reserve. [DOESNT WORK]"></i></div>
+                    </div>
+                    <div className="row" style={{height:"80%"}}>
+                        <div className="col-12">
+                            <img src={eventScreen} className="img-fluid"alt="roomImage"/>
+                        </div>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div style={{display:"block",width:"100%",height:"100%"}} className="container">
+                    <div className="row" style={{height:"10%",paddingTop:10}}>
+                        <div className="col-6" style={{height:"100%"}}>
+                            <form >
+                                <label>
+                                    <p> From <select value={this.state.value} onChange={this.handleChangeFrom}>
+                                        <option value="12am">12am</option>
+                                        <option value="1pm">1pm</option>
+                                        <option value="2pm">2pm</option>
+                                        <option value="3pm">3pm</option>
+                                        <option value="4pm">4pm</option>
+                                        <option value="5pm">5pm</option>
+                                        <option value="6pm">6pm</option>
+                                        <option value="7pm">7pm</option>
+                                    </select> </p>
+                                </label>
+                            </form>
+                        </div>
+                        <div className="col-6" style={{height:"100%"}}>
+                            <form >
+                                <label>
+                                    <p> To <select value={this.state.value} onChange={this.handleChangeTo}>
+                                        <option value="12am">12am</option>
+                                        <option value="1pm">1pm</option>
+                                        <option value="2pm">2pm</option>
+                                        <option value="3pm">3pm</option>
+                                        <option value="4pm">4pm</option>
+                                        <option value="5pm">5pm</option>
+                                        <option value="6pm">6pm</option>
+                                        <option value="7pm">7pm</option>
+                                    </select> </p>
+                                </label>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="row" style={{height:"89.9%",overflowY:"scroll",width:"100%"}}>
+                        <div className="col-12">
+                            <li style={{listStyle:"none",margin:10}}>
+                                {this.state.list.map( (ele,index) =>
+                                    <ul key={index}><button className="roomIconButton" style={{marginTop:5}} onClick={this.handleRoomBoolean}>{ele.key}</button><img src={ele.value} alt="test" style={{width:"70%"}} /></ul>
+                                )}
+                            </li>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
 	
 	
 	
     render() {
-		
-		
         var displayStyle = {
-            position: "relative",
-        };
-		
-		var submitButton = {
-				position: "absolute",
-				top: "50%",
-				left: "50%",
-				transform: "translate(-50%, -50%)",
-				fontSize: "5px",
-				width: "5%",
-				height: "5%",
-				fontFamily: "sans-serif",
-				
-		};
-		
-		
-		
-		// eslint-disable-next-line
-		
+            display: "block",
+            weight: "100%",
+            height: "100%",
+            //overflow: "hidden"
+        }
         return (
-			<div class="container" style={displayStyle}>
-			<img id = "calendar_img" src="eventimgs/calendar.jpg" alt="PH" ></img>
-				<div class="row">
-					<div class="col-sm-6">
-						<div class="button-group" style={submitButton}>
-							<button id = "pizza_button" onClick={this.goToEvent} class="myButton">Pizza Party</button>
-						</div>
-					</div>
-				</div>
-				 <div class="col-sm-6">
-				 </div>
-			</div>
-			
+            <div style={displayStyle} >
+                {this.showPop()}
+            </div>
         );
     }
 
