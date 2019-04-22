d65e3befa96ca952f0a75c32d8d6633238a1ebf0
mercyspirit <arden@DESKTOP-RFP700C.localdomain>
2019-04-21 23:59:17 -0400

    added more images

--- kiosk/src/classes.js
+++ kiosk/src/classes.js
@@ -3,21 +3,47 @@ import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 
 class Classes extends Component {
-
+	constructor(props) {
+		super(props);
+		this.state = {floor: 0, room: 0, imgURL: '/images/ground0000.png'};
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
 
     render() {
         var displayStyle = {
             display: "block",
-            weight: "100%",
-            height: "100%",
+            width: "75%",
             overflow: "hidden"
         }
         return (
-            <div style={displayStyle}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpq8T9bdcQpy2myWNZhLI5m6YGbyugIPV-FY26kj17-W9Jo6Kj" alt="PH"></img>
-            </div>
-        );
+		<div>
+			<select onChange = {this.handleChange}>
+		  		<option value="ground0000">Ground Floor</option>
+		  		<option value="first0000">Floor 1</option>
+		  		<option value="na">Floor 2</option>
+		 		<option value="na">Floor 3</option>
+		  		<option value="na">Floor 4</option>
+		  		<option value="na">Floor 5</option>
+			</select>
+			<form onSubmit={this.handleSubmit}>
+				<input type = "text"/>
+				<button type="submit"><i class="fa fa-search"></i></button><div class="dropdown">
+		</div>
+		</form>
+            <img id = "map" src = {process.env.PUBLIC_URL + this.state.imgURL} style = {displayStyle}/>
+        </div>
+	);
     }
+	handleSubmit(e) {
+	e.preventDefault();
+		console.log("submit");
+	}
+
+	handleChange(e) {
+		console.log(e.target.value);
+		this.setState({imgURL: '/images/' + e.target.value + '.png'});
+	}
 
 }
 
